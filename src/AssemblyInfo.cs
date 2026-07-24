599538625defd55a655a7b7e7132e2131527b839
Leonard-china <thaophucpham79@gmail.com>
2026-07-24 11:50:55 +0800

    Add src/AssemblyInfo.cs

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("TranslationByLocalAI")]
+[assembly: AssemblyDescription("Windows local AI instant translation tool powered by llama.cpp")]
+[assembly: AssemblyCompany("Leonard-china")]
+[assembly: AssemblyProduct("TranslationByLocalAI")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
